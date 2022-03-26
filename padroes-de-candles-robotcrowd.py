98afd7408595ba8f66db818d4837b774b426002c
Leonardo Araújo Bezerra <61925255+leoabpi@users.noreply.github.com>
2022-03-26 10:34:08 -0300

    Update padroes-de-candles-robotcrowd.py
    Ajustes

--- padroes-de-candles-robotcrowd.py
+++ padroes-de-candles-robotcrowd.py
@@ -1,6 +1,6 @@
-alta = (C0) > (O0)
-baixa = (C0) < (O0)
-cog_alta = (C0) < (O0) & (C1) > (O1)
-cog_baixa = (C0) > (O0) & (C1) < (O1)
-cogm_alta = (C0) < (O0) & (C1) > (O1) & (L0) == H1)
-cogm_baixa = (C0) > (O0) & (C1) < (O1) & (L1) == H0)
+alta = (C0) > (O0) #candle de alta
+baixa = (C0) < (O0) #candle de baixa
+cog_alta = (C0) < (O0) & (C1) > (O1) #candle de baixa seguido de candle de alta
+cog_baixa = (C0) > (O0) & (C1) < (O1) #candle de alta seguido de candle de baixa
+cogm_alta = (C0) < (O0) & (C1) > (O1) & (L0) == (H1) #candle de baixa seguido de candle de alta
+cogm_baixa = (C0) > (O0) & (C1) < (O1) & (L1) == (H0) #candle de alta seguido de candle de baixa
